4e7ef406681cd4cb5e66dda3a99cb39c39944b94
anikamorris <anikamorris11@gmail.com>
2019-10-10 17:12:53 -0700

    color swatches functioning

--- app.py
+++ app.py
@@ -18,16 +18,25 @@ def products_index():
     r = requests.get("http://makeup-api.herokuapp.com/api/v1/products.json?product_type=nail_polish")
 
     json_data = r.json()
-    for nail_polish in json_data:
-        db.nail_polishes.insert_one(nail_polish)
+    print('count', nail_polishes.count({}))
+    if nail_polishes.count({}) == 0:
+        for nail_polish in json_data:
+            nail_polishes.insert_one(nail_polish)
 
     return render_template('products_index.html', nail_polishes=nail_polishes.find())
+
+# https://gist.github.com/matthewkremer/3295567
+def hex_to_rgb(hex):
+    hex = hex.lstrip('#')
+    hlen = len(hex)
+    return tuple(int(hex[i:i+hlen//3], 16) for i in range(0, hlen, hlen//3))
 
 @app.route('/products/<product_id>')
 def products_show(product_id):
     nail_polish = nail_polishes.find_one({'_id': ObjectId(product_id)})
-    return render_template('products_show.html', nail_polish=nail_polish)
+    colors = []
+    for color in nail_polish["product_colors"]:
+        rgb = hex_to_rgb(color["hex_value"])
+        colors.append([rgb, color["colour_name"]])
+    return render_template('products_show.html', nail_polish=nail_polish, colors=colors)
 
-@app.route('/cart')
-def show_cart():
-    return render_template('cart.html')
